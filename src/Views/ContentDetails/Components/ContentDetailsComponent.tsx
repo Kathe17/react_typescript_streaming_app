1237414604878398f705ac8c82811988485a8836
Kathe17 <katherey97@gmail.com>
2024-03-08 17:06:31 -0500

    ContentDetails

--- src/Views/ContentDetails/Components/ContentDetailsComponent.tsx
+++ src/Views/ContentDetails/Components/ContentDetailsComponent.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import useDetails from "../../../Hooks/useDetails";
 import { DetailsModel } from "../../../Models/Details.model";
-import { baseUrlImages } from "../../../Share/Constants";
+import { VscDebugBreakpointData } from "react-icons/vsc";
 
 const ContentDetailsComponent = ({
   detailId,
@@ -33,13 +33,23 @@ const ContentDetailsComponent = ({
         backgroundSize: "100% 100%",
       }}
     >
-      <div className="flex flex-col w-2/5 h-full bg-yellow-300">
-        <img
-          className="h-2/5"
-          src={`${baseUrlImages}${getFirstDetail(details)?.logo}`}
-          alt=""
-        />
-        {getFirstDetail(details)?.bgImage}
+      <div className="flex flex-col items-center w-2/5 h-full bg-yellow-300">
+        <div className="flex flex-col justify-between h-2/3 bg-blue-300 p-2">
+          <img className="w-1/5" src={getFirstDetail(details)?.logo} alt="" />
+          <div className="flex w-1/6 bg-slate-300 rounded-lg justify-center">
+            <p>{getFirstDetail(details)?.rating}</p>
+          </div>
+          <div className="flex items-center">
+            <p>{getFirstDetail(details)?.releaseDate}</p>
+            <VscDebugBreakpointData />
+            <p>{getFirstDetail(details)?.duration}</p>
+            <VscDebugBreakpointData />
+            {getFirstDetail(details)?.categories.map((category) => (
+              <div>{category.nombre}.</div>
+            ))}
+          </div>
+        </div>
+        <p>{getFirstDetail(details)?.synopsis}</p>
       </div>
       {/* <div className="grid grid-cols-4 gap-4 h-5/6">
             {details?.map((detail) => (
