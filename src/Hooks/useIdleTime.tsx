835b8ca0a9189ba22d920a48daa0afa75b080142
Katherine Rey <katherey97@gmail.com>
2024-08-28 20:13:10 -0500

    Update useIdleTime.tsx

--- src/Hooks/useIdleTime.tsx
+++ src/Hooks/useIdleTime.tsx
@@ -13,16 +13,16 @@ export default function useIdle({ onIdle, idleTime }: Props) {
   const [isIdle, setIsIdle] = useState<boolean>(false);
 
   const handleOnIdle = () => {
-    setIsIdle(true);
-    getLastActiveTime();
-    onIdle?.();
+    setIsIdle(true); // Seteo el estado de isIdle a True
+    getLastActiveTime(); // Función de la librería react-idle-timer para obtener el último momento en que estuvo activo
+    onIdle?.(); // Funcion que debe hacer cuando esté inactivo
   };
 
   const { getRemainingTime, getLastActiveTime } = useIdleTimer({
     timeout: 1000 * 60 * idleTime,
     onIdle: handleOnIdle,
-    debounce: 500,
-    crossTab: true,
+    debounce: 500, // Tiempo en milisegundos antes de volverla a ejecutar
+    crossTab: true, // Toma en cuenta el tiempo en el que se encuentra en otras pestañas o pantallas.
   });
 
   return {
